49161a12e59723be73392ad2ed85483d0c599a09
Jorge André Florentino Collado <andre.florentino.c@gmail.com>
2024-01-18 12:19:44 +0000

    Bash script working

--- guessingname.sh
+++ guessingname.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
 # File: guessingname.sh
 
+function verify_guess {
+    local response="high"
+    
+    if [[ $1 -eq $(ls | wc -l) ]]
+    then
+        response="equal"
+    elif [[ $1 -lt $(ls | wc -l) ]]
+    then
+        response="low"
+    fi
+
+    echo $response
+}
+
 while [[ 5 -eq 5 ]]
 do
-    echo "Enter the number of files in this directory: "
 
+    echo "Enter the number of files in this directory: "
     read user_guess
-    
-    if [[ $(verify_guess $user_guess)="equal" ]]
-    then
+
+    if [[ $(verify_guess $user_guess) == "equal" ]]; then
 
         echo "Congratulations, you guessed it!"
         break
 
-    else if [[ $(verify_guess $user_guess)="low" ]]
+    elif [[ $(verify_guess $user_guess) == "low" ]]
     then
 
         echo "That's a bit too low..."
@@ -24,19 +37,3 @@ do
 
     fi
 done
-
-function verify_guess {
-    local response=""
-
-    if [[ $1 -eq $(ls | wc -l) ]]
-    then
-        let response="equal"
-    else if [[ $1 -lt $(ls | wc -l) ]]
-    then
-        let response="low"
-    else
-        let response="high"
-    fi
-
-    echo $response
-}
